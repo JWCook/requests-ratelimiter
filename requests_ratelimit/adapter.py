7ca67da259b969b24bd38626187d6dc033e21efd
Jordan Cook <jordan.cook@pioneer.com>
2021-09-04 12:42:09 -0500

    Take Limiter arguments instead of a Limiter object

--- requests_ratelimit/adapter.py
+++ requests_ratelimit/adapter.py
@@ -1,28 +1,34 @@
+from typing import Type
 from urllib.parse import urlparse
 from uuid import uuid4
 
 from pyrate_limiter import Limiter
+from pyrate_limiter.bucket import AbstractBucket, MemoryQueueBucket
+from pyrate_limiter.request_rate import RequestRate
 from requests.adapters import HTTPAdapter
 
 
-# TODO: Take a Limiter object, or the same args as Limiter.__init__()?
 class LimiterAdapter(HTTPAdapter):
     """Transport adapter that adds rate-limiting"""
 
-    def __init__(self, limiter: Limiter = None, per_host: bool = False, **kwargs):
-        self.limiter = limiter
+    # def __init__(self, limiter: Limiter = None, per_host: bool = False, **kwargs):
+    #     self.limiter = limiter
+    #     self.per_host = per_host
+    #     self._default_bucket = str(uuid4())
+    #     super().__init__(**kwargs)
+
+    def __init__(
+        self,
+        *rates: RequestRate,
+        bucket_class: Type[AbstractBucket] = MemoryQueueBucket,
+        bucket_kwargs=None,
+        per_host: bool = False,
+        **kwargs,
+    ):
+        self.limiter = Limiter(*rates, bucket_class=bucket_class, bucket_kwargs=bucket_kwargs)
         self.per_host = per_host
         self._default_bucket = str(uuid4())
         super().__init__(**kwargs)
-
-    # def __init__(
-    #     self,
-    #     *rates: RequestRate,
-    #     bucket_class: Type[AbstractBucket] = MemoryQueueBucket,
-    #     bucket_kwargs=None,
-    #     **kwargs,
-    # ):
-    #     self.limiter = Limiter(*rates, bucket_class, bucket_kwargs)
 
     def bucket_name(self, request):
         return urlparse(request.url).netloc if self.per_host else self._default_bucket
