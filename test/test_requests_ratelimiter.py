a679a1ce1f20a27cbe4c1d26736838f8b2e66433
Jordan Cook <jordan.cook.git@proton.me>
2026-02-27 18:20:30 -0600

    Combine some tests with parametrize

--- test/test_requests_ratelimiter.py
+++ test/test_requests_ratelimiter.py
@@ -26,10 +26,25 @@ from test.conftest import (
 patch_sleep = patch('pyrate_limiter.limiter.sleep', side_effect=sleep)
 
 
+class CustomSession(LimiterMixin, Session):
+    """Custom Session that adds an extra class attribute"""
+
+    def __init__(self, *args, flag: bool = False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.flag = flag
+
+
 @patch_sleep
-def test_limiter_session(mock_sleep):
-    session = LimiterSession(per_second=5)
-    session = mount_mock_adapter(session)
+@pytest.mark.parametrize(
+    'session_factory',
+    [
+        lambda: LimiterSession(per_second=5),
+        lambda: CustomSession(per_second=5, flag=True),
+    ],
+    ids=['LimiterSession', 'CustomSession'],
+)
+def test_rate_limit_enforcement(mock_sleep, session_factory):
+    session = mount_mock_adapter(session_factory())
 
     for _ in range(5):
         session.get(MOCKED_URL)
@@ -37,6 +52,11 @@ def test_limiter_session(mock_sleep):
 
     session.get(MOCKED_URL)
     assert mock_sleep.called is True
+
+
+def test_custom_session_preserves_attributes():
+    session = CustomSession(per_second=5, flag=True)
+    assert session.flag is True
 
 
 @patch_sleep
@@ -65,38 +85,24 @@ def test_custom_limiter(mock_sleep):
     assert mock_sleep.called is True
 
 
-class CustomSession(LimiterMixin, Session):
-    """Custom Session that adds an extra class attribute"""
-
-    def __init__(self, *args, flag: bool = False, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.flag = flag
-
-
 @patch_sleep
-def test_custom_session(mock_sleep):
-    session = CustomSession(per_second=5, flag=True)
-    session = mount_mock_adapter(session)
-    assert session.flag is True
+@pytest.mark.parametrize(
+    'url, session_kwargs, expect_sleep',
+    [
+        (MOCKED_URL_429, {}, True),  # default: 429 fills bucket
+        (MOCKED_URL_500, {'limit_statuses': [500]}, True),  # custom status fills bucket
+        (MOCKED_URL_429, {'limit_statuses': []}, False),  # disabled: no fill on 429
+    ],
+)
+def test_limit_status_handling(mock_sleep, url, session_kwargs, expect_sleep):
+    """Bucket is filled (or not) depending on the response status and limit_statuses config"""
+    session = get_mock_session(per_second=5, **session_kwargs)
 
-    for _ in range(5):
-        session.get(MOCKED_URL)
+    session.get(url)
     assert mock_sleep.called is False
 
-    session.get(MOCKED_URL)
-    assert mock_sleep.called is True
-
-
-@patch_sleep
-def test_429(mock_sleep):
-    """After receiving a 429 response, the bucket should be filled, allowing no more requests"""
-    session = get_mock_session(per_second=5)
-
-    session.get(MOCKED_URL_429)
-    assert mock_sleep.called is False
-
-    session.get(MOCKED_URL_429)
-    assert mock_sleep.called is True
+    session.get(url)
+    assert mock_sleep.called is expect_sleep
 
 
 @patch_sleep
@@ -108,28 +114,6 @@ def test_429__per_host(mock_sleep):
 
     # A 429 from one host should not affect requests for a different host
     session.get(MOCKED_URL_ALT_HOST)
-    assert mock_sleep.called is False
-
-
-@patch_sleep
-def test_custom_limit_status(mock_sleep):
-    """Optionally handle additional status codes that indicate an exceeded rate limit"""
-    session = get_mock_session(per_second=5, limit_statuses=[500])
-
-    session.get(MOCKED_URL_500)
-    assert mock_sleep.called is False
-
-    session.get(MOCKED_URL_500)
-    assert mock_sleep.called is True
-
-
-@patch_sleep
-def test_limit_status_disabled(mock_sleep):
-    """Optionally handle additional status codes that indicate an exceeded rate limit"""
-    session = get_mock_session(per_second=5, limit_statuses=[])
-
-    session.get(MOCKED_URL_429)
-    session.get(MOCKED_URL_429)
     assert mock_sleep.called is False
 
 
